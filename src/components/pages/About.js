248b1a152d7791631920069085dc013308052a8b
Ronald Hajewski <ronald.hajewski@gmail.com>
2019-02-27 17:04:54 -0600

    completed another React todo

---
+++ src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>
+        This is the TodoList app v1.1.0. It is the second time I'm building
+        this.
+      </p>
+    </React.Fragment>
+  );
+}
+
+export default About;
